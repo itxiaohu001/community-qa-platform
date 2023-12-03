594f2ac62f9fd1b49da5a69b011ac805460b331a
xiaohu <578209081@qq.com>
2023-12-03 14:46:20 +0800

    add tests

---
+++ tests/repo/user_repo_test.go
@@ -0,0 +1,29 @@
+package repo
+
+import (
+	"community-qa-platform/internal/config"
+	"community-qa-platform/internal/models"
+	"community-qa-platform/internal/repository"
+	"community-qa-platform/pkg/database"
+	"testing"
+)
+
+func NewDefaultRepo() *repository.UserRepo {
+	dbConf := config.InitConfig("./config.json")
+	database.InitDB(dbConf.Db)
+	return repository.NewUserRepo(database.DB)
+}
+
+func TestCreateUserRepo(t *testing.T) {
+	repo := NewDefaultRepo()
+
+	user := &models.User{
+		UserName: "xiaohu4",
+		PassWord: "fsafdasdf",
+		Email:    "test4@qq.com",
+	}
+	if err := repo.CreateUserRepo(user); err != nil {
+		t.Error(err)
+		return
+	}
+}
